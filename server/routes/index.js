bb059185d315146da9d21151f4dee5a488822122
Evans Ansong <65941289+evans646@users.noreply.github.com>
2022-10-08 13:43:22 -0500

    feat:removed unwanted files

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,11 @@ import {tasksRoute} from   './tasksRoute';
 import {createTaskRoute} from './createTaskRoute';
 import { completeTaskRoute} from './completeTaskRoute';
 import {removeTaskRoute} from './removeTaskRoute';
-import { testRoute } from './testRoute';
+
 
 export const routes = [
     tasksRoute,
     createTaskRoute,
     completeTaskRoute, 
     removeTaskRoute,
-    testRoute
 ];
